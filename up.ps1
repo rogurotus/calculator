477f42e4c356c17413e7392ea4957ce891cb29a1
rogurotus <rogurotus@gmail.com>
2022-07-06 11:58:19 +0300

    test

---
+++ up.ps1
@@ -0,0 +1,12 @@
+$env:RUST_BACKTRACE=1;
+$env:RUST_LOG="warn";
+$env:COMPOSE_MEDEA_IMAGE_NAME="hub.instrumentisto.com/streaming/medea"; 
+$env:COMPOSE_MEDEA_IMAGE_VER="edge";
+$env:COMPOSE_CONTROL_MOCK_IMAGE_VER="dev";
+$env:COMPOSE_WEBDRIVER_IMAGE_NAME="selenoid/chrome";
+$env:COMPOSE_WEBDRIVER_IMAGE_VER="102.0";
+$env:COMPOSE_WEBDRIVER_ENTRYPOINT="/entrypoint.sh";
+# $env:COMPOSE_CONTROL_MOCK_IMAGE_NAME="instrumentisto/medea-control-api-mock"
+
+# docker-compose -f ./docker-compose.yml logs -f 
+docker-compose -f ./docker-compose.yml up
